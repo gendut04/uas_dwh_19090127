7a1fc3778628205a6e0d0784272396ca4b6e6cfe
gendut04 <miftakhulanam30@gmail.com>
2022-01-09 13:10:24 +0700

    UAS DWH

---
+++ spk_ahp/database/ahp.sql
@@ -0,0 +1,304 @@
+-- phpMyAdmin SQL Dump
+-- version 5.1.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Jan 09, 2022 at 07:00 AM
+-- Server version: 10.4.21-MariaDB
+-- PHP Version: 7.4.24
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `ahp`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `alternatif`
+--
+
+CREATE TABLE `alternatif` (
+  `id` int(11) NOT NULL,
+  `nama` varchar(20) NOT NULL
+) ENGINE=MyISAM DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `alternatif`
+--
+
+INSERT INTO `alternatif` (`id`, `nama`) VALUES
+(21, 'Perumahan Griya Satr'),
+(22, 'Perumahan Sapphire R'),
+(23, 'Perumahan 1001 Pintu');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `ir`
+--
+
+CREATE TABLE `ir` (
+  `jumlah` int(11) NOT NULL,
+  `nilai` float NOT NULL
+) ENGINE=MyISAM DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `ir`
+--
+
+INSERT INTO `ir` (`jumlah`, `nilai`) VALUES
+(1, 0),
+(2, 0),
+(3, 0.58),
+(4, 0.9),
+(5, 1.12),
+(6, 1.24),
+(7, 1.32),
+(8, 1.41),
+(9, 1.45),
+(10, 1.49),
+(11, 1.51),
+(12, 1.48),
+(13, 1.56),
+(14, 1.57),
+(15, 1.59);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `kriteria`
+--
+
+CREATE TABLE `kriteria` (
+  `id` int(11) NOT NULL,
+  `nama` varchar(20) NOT NULL
+) ENGINE=MyISAM DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `kriteria`
+--
+
+INSERT INTO `kriteria` (`id`, `nama`) VALUES
+(33, 'Tipe'),
+(32, 'Lokasi'),
+(31, 'Harga');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `perbandingan_alternatif`
+--
+
+CREATE TABLE `perbandingan_alternatif` (
+  `id` int(11) NOT NULL,
+  `alternatif1` int(11) NOT NULL,
+  `alternatif2` int(11) NOT NULL,
+  `pembanding` int(11) NOT NULL,
+  `nilai` float NOT NULL
+) ENGINE=MyISAM DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `perbandingan_alternatif`
+--
+
+INSERT INTO `perbandingan_alternatif` (`id`, `alternatif1`, `alternatif2`, `pembanding`, `nilai`) VALUES
+(19, 21, 23, 31, 1),
+(18, 21, 22, 31, 1),
+(20, 22, 23, 31, 0.5),
+(21, 21, 22, 32, 2),
+(22, 21, 23, 32, 1),
+(23, 22, 23, 32, 1),
+(24, 21, 22, 33, 2),
+(25, 21, 23, 33, 1),
+(26, 22, 23, 33, 1);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `perbandingan_kriteria`
+--
+
+CREATE TABLE `perbandingan_kriteria` (
+  `id` int(11) NOT NULL,
+  `kriteria1` int(11) NOT NULL,
+  `kriteria2` int(11) NOT NULL,
+  `nilai` float NOT NULL
+) ENGINE=MyISAM DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `perbandingan_kriteria`
+--
+
+INSERT INTO `perbandingan_kriteria` (`id`, `kriteria1`, `kriteria2`, `nilai`) VALUES
+(12, 32, 33, 1),
+(11, 31, 33, 2),
+(10, 31, 32, 1);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `pv_alternatif`
+--
+
+CREATE TABLE `pv_alternatif` (
+  `id` int(11) NOT NULL,
+  `id_alternatif` int(11) NOT NULL,
+  `id_kriteria` int(11) NOT NULL,
+  `nilai` float NOT NULL
+) ENGINE=MyISAM DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `pv_alternatif`
+--
+
+INSERT INTO `pv_alternatif` (`id`, `id_alternatif`, `id_kriteria`, `nilai`) VALUES
+(47, 21, 32, 0.411111),
+(46, 23, 31, 0.411111),
+(45, 22, 31, 0.261111),
+(44, 21, 31, 0.327778),
+(48, 22, 32, 0.261111),
+(49, 23, 32, 0.327778),
+(50, 21, 33, 0.411111),
+(51, 22, 33, 0.261111),
+(52, 23, 33, 0.327778);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `pv_kriteria`
+--
+
+CREATE TABLE `pv_kriteria` (
+  `id_kriteria` int(11) NOT NULL,
+  `nilai` float NOT NULL
+) ENGINE=MyISAM DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `pv_kriteria`
+--
+
+INSERT INTO `pv_kriteria` (`id_kriteria`, `nilai`) VALUES
+(33, 0.261111),
+(32, 0.327778),
+(31, 0.411111);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `ranking`
+--
+
+CREATE TABLE `ranking` (
+  `id_alternatif` int(11) NOT NULL,
+  `nilai` float NOT NULL
+) ENGINE=MyISAM DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `ranking`
+--
+
+INSERT INTO `ranking` (`id_alternatif`, `nilai`) VALUES
+(22, 0.261111),
+(21, 0.376852),
+(23, 0.362037);
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `alternatif`
+--
+ALTER TABLE `alternatif`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `ir`
+--
+ALTER TABLE `ir`
+  ADD PRIMARY KEY (`jumlah`);
+
+--
+-- Indexes for table `kriteria`
+--
+ALTER TABLE `kriteria`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `perbandingan_alternatif`
+--
+ALTER TABLE `perbandingan_alternatif`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `perbandingan_kriteria`
+--
+ALTER TABLE `perbandingan_kriteria`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `pv_alternatif`
+--
+ALTER TABLE `pv_alternatif`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `pv_kriteria`
+--
+ALTER TABLE `pv_kriteria`
+  ADD PRIMARY KEY (`id_kriteria`);
+
+--
+-- Indexes for table `ranking`
+--
+ALTER TABLE `ranking`
+  ADD PRIMARY KEY (`id_alternatif`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `alternatif`
+--
+ALTER TABLE `alternatif`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=24;
+
+--
+-- AUTO_INCREMENT for table `kriteria`
+--
+ALTER TABLE `kriteria`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=34;
+
+--
+-- AUTO_INCREMENT for table `perbandingan_alternatif`
+--
+ALTER TABLE `perbandingan_alternatif`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=27;
+
+--
+-- AUTO_INCREMENT for table `perbandingan_kriteria`
+--
+ALTER TABLE `perbandingan_kriteria`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=13;
+
+--
+-- AUTO_INCREMENT for table `pv_alternatif`
+--
+ALTER TABLE `pv_alternatif`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=53;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
